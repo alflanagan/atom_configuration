0af55a7c2c897ca93bd143159e65e7fd25a15915
alflanagan <a.lloyd.flanagan@gmail.com>
2015-10-31 15:31:02 -0400

    Add --beta, fix dependcy handling

--- atom_setup.py
+++ atom_setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# vim:ts=4:sw=4:expandtab:
+# vim: sw=4:ts=4:expandtab
 """A script to get the list of installed atom packages, match it to the list of
 expected packages, and install any that are missing."""
 
@@ -13,7 +13,14 @@ DEPENDENCIES = {"nuclide-installer":
                 ("nuclide-clang-atom", "nuclide-blame-ui", "nuclide-diagnostics-ui",
                  "nuclide-debugger-atom", "nuclide-debugger-hhvm", "nuclide-arcanist",
                  "nuclide-diagnostics-store", "nuclide-debugger-lldb", "nuclide-blame-provider-hg",
-                 "nuclide-diff-view", "nuclide-blame", "nuclide-code-format", "nuclide-buck-files")}
+                 "nuclide-diff-view", "nuclide-blame", "nuclide-code-format", "nuclide-buck-files",
+                 "nuclide-hack-symbol-provider", "nuclide-hack", "nuclide-fuzzy-filename-provider",
+                 "nuclide-file-watcher", "nuclide-test-runner", "nuclide-flow", "nuclide-toolbar",
+                 "nuclide-file-tree-deux", "nuclide-objc", "nuclide-find-references",
+                 "nuclide-open-filenames-provider", "nuclide-move-pane", "nuclide-type-hint",
+                 "nuclide-file-tree", "nuclide-ocaml", "nuclide-hg-repository",
+                 "nuclide-remote-projects", "nuclide-quick-open", "nuclide-language-hack",
+                 "nuclide-format-js", )}
 
 
 def get_args():
@@ -22,11 +29,13 @@ def get_args():
         description="Report or install apm packages according to my-packages.txt")
     parser.add_argument('--install', action='store_true',
                         help="Actually install missing packages (default: report only).")
+    parser.add_argument('--beta', action='store_true',
+                        help="Act on beta-channel atom version.")
     return parser.parse_args()
 
 
-def get_installed_pkgs():
-    apm_list = subprocess.Popen(['apm', 'list', '-ib'], stdout=subprocess.PIPE, bufsize=-1)
+def get_installed_pkgs(apm_prog):
+    apm_list = subprocess.Popen([apm_prog, 'list', '-ib'], stdout=subprocess.PIPE, bufsize=-1)
     results, _ = apm_list.communicate()
 
     installed = set()
@@ -34,6 +43,13 @@ def get_installed_pkgs():
         parts = spec.split(b"@")
         if parts[0]:
             installed.add(parts[0].decode('utf-8'))
+
+    # remove dependencies from list
+    for key in DEPENDENCIES:
+        if key in installed:
+            for pkg in DEPENDENCIES[key]:
+                installed.discard(pkg)
+
     return installed
 
 
@@ -42,19 +58,15 @@ def get_wanted_packages():
     with open('my-packages.txt', 'r') as myin:
         for line in myin:
             wanted.add(line[:-1])
-    for key in DEPENDENCIES:
-        if key in wanted:
-            for pkg_name in DEPENDENCIES[key]:
-                wanted.add(pkg_name)
     return wanted
 
 
-def install_missing(wanted, installed):
+def install_missing(apm_prog, wanted, installed):
     to_install = wanted - installed
     if to_install:
         print("Installing {} missing packages.".format(len(to_install)))
         for pkg in wanted - installed:
-            subprocess.call(['apm', 'install', pkg])
+            subprocess.call([apm_prog, 'install', pkg])
     else:
         print("nothing to do!")
 
@@ -77,12 +89,13 @@ def report_extra_packages(wanted, installed):
 
 def main():
     args = get_args()
-    installed = get_installed_pkgs()
+    apm = 'apm-beta' if args.beta else 'apm'
+    installed = get_installed_pkgs(apm)
     wanted = get_wanted_packages()
     report_missing_packages(wanted, installed)
     report_extra_packages(wanted, installed)
     if args.install:
-        install_missing(wanted, installed)
+        install_missing(apm, wanted, installed)
 
 
 if __name__ == '__main__':
