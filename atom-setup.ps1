89e4d08324c47f6d6a54494a86d52bf285d9eb99
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2017-01-11 16:06:55 -0500

    working version of pkg installer for powershell

--- atom-setup.ps1
+++ atom-setup.ps1
@@ -1,3 +1,35 @@
-﻿$fred = (Get-Content my-packages.txt)
-$env:PYTHON = "C:\Python27\python.exe"
-ForEach-Object -InputObject $fred { apm install $_ }
+# for node-gyp
+$env:PYTHON = "C:\Python27\pythonw.exe"
+
+$fred = (Get-Content my-packages.txt)
+$wilma = apm list --no-color --bare --installed
+$installed = $wilma.ForEach( { $_.split('@')[0] } )
+$differences = Compare-Object -ReferenceObject $installed -DifferenceObject $fred
+
+# Compare-Object returns PSCustomObject if collections are equal (???)
+# but is schizo about type of return array
+if ($differences.GetType().Name -ne "Object[]" -and $differences.GetType().Name -ne "Array")
+{
+  Write-Host "There are no differences between installed and my-packages.txt"  -ForegroundColor Cyan
+  exit 0
+}
+
+$uninstalled = $differences.Where( { $_.SideIndicator -eq "=>" } )
+$uninstalled = $uninstalled.ForEach( { $_.InputObject })
+$extra = $differences.Where( { $_.SideIndicator -eq "<=" } )
+$extra = $extra.ForEach( { $_.InputObject } )
+
+if ($uninstalled.Count -gt 1 -or $extra[0] -ne "") {
+  Write-Host "Packages In my-packages Not Installed" -ForegroundColor Red
+  Write-Host $uninstalled
+  Write-Host "`n"
+}
+
+if ($extra.Count -gt 1 -or $extra[0] -ne "") {
+  Write-Host "Packages Installed But Not in my-packages List" -ForegroundColor Red
+  Write-Host $extra
+}
+
+if ($args.Contains("--install")) {
+  $uninstalled.ForEach( { apm install --no-color $_ } )
+}
